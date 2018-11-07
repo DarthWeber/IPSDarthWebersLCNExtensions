28e5f6e825604c3d41e39588e97d866f53aa650a
DarthWeber <tom.weber@gmx.de>
2018-11-07 06:14:15 +0100

    Add files via upload

--- LCNTastensperre/module.php
+++ LCNTastensperre/module.php
@@ -26,6 +26,7 @@
 			
 			//Apply filter
 			//$this->SetReceiveDataFilter($this->ReadPropertyString("ReceiveFilter"));
+      IPS_LogMessage("IOTest", "Inititalisiere Filter M000".$this->RegisterPropertyInteger("ModulID"));
 			$this->SetReceiveDataFilter(".*=M000022\.TX[0-9]{12}.*");
 			
 		}
