d504679c6a4264354a3c7547debad0cb1c3e3eaa
DarthWeber <tom.weber@gmx.de>
2018-11-10 06:34:25 +0100

    Add files via upload

--- LCNTastensperre/module.php
+++ LCNTastensperre/module.php
@@ -46,11 +46,7 @@
 		  	$this->SendDebug("FUNCTION -Update-", "Kernel is not ready! Kernel Runlevel = ".IPS_GetKernelRunlevel(), 0);
 			  return false;
   		}
-    try {
-      $this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => utf8_encode(">M".sprintf("%06d",$this->ReadPropertyInteger("ModulID")).".TX\n"))));
-      } catch (Exception $e) {
-      // Nothing, this is normal
-      }
+      @$this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => utf8_encode(">M".sprintf("%06d",$this->ReadPropertyInteger("ModulID")).".TX\n"))));
 		}
 		
 		public function ReceiveData($JSONString)
