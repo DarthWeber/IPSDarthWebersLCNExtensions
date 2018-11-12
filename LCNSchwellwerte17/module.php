913f5f3f10d81a6d1774ac92a64fd37c8b949d52
DarthWeber <tom.weber@gmx.de>
2018-11-12 17:17:36 +0100

    Add files via upload

--- LCNSchwellwerte17/module.php
+++ LCNSchwellwerte17/module.php
@@ -46,7 +46,7 @@
               $this->ReadPropertyInteger('ModulID') .
               ',"Function":"TX".*';
       $this->SendDebug('FILTER', $Filter, 0);
-      $this->SetReceiveDataFilter($Filter);
+      $this->SetReceiveDataFilter(".*");
 	 		$this->SetTimerInterval("SendSECommand", $this->ReadPropertyInteger("Intervall") * 1000);
 		}
 
